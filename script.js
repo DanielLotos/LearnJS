f21ef739e8c2633d73305332017f364572eb05cf
DanielLotos <daniellika@yandex.ru>
2018-08-01 19:14:20 +0300

    trash code but work

--- script.js
+++ script.js
@@ -1,109 +1,104 @@
-// let weekend = ['Monday', 'Tuesday', 'Wednesday','Thusday','Friday'];
-
-// Применили метод .map.
-// Метод .map возвращает новый массив,
-// каждый элемент которого формируется из значений,
-// которые возвращаются из функции,
-// которую мы передаем в качестве первого аргумента.
-// Своими словами:
-// Для каждого элемента старого массива срабатывает функция и
-// создается новый массив из того, что вернула (return) функция.
-// document.body.innerHTML = weekend.map(function(e) { return e.toUpperCase() });
-// MONDAY,TUESDAY,WEDNESDAY,THUSDAY,FRIDAY
-
-
-
-// Применили метод .every
-// Метод .every проверяет каждый элемент массива на правило,
-// которое мы задаем в первом аргументе - функции.
-// Если все элементы соответствуют правилу, то возвращает true,
-// иначе возвращает false
-// let array = ["Some string", "Another string", "Third string",
-//   "JavaScript", "Sorax"];
-// console.log(array.every(function(e) { return e.length > 4 }));
-// true
-
-
-
-// Применили метод .some
-// Метод .some аналогичен методу .every,
-// но возвращает true если ХОТЯ БЫ ОДИН элемент
-// соответствует правилу.
-// Если таких элементов вообще нет, то false
-// console.log(array.some(function(e) { return e.length > 10 }));
-// true, т.к. есть хотя бы одна строка, имеющая длину
-// больше 10-ти символов
-
-
-
-
-
-
-
-
-
-
-
-
 let Product, product;
+let typeItem;
+let typeButtons = document.querySelectorAll('.product-header__btn');
 
-Product = function(category, name, src, price) {
-  this.category = category;
+let productList = document.getElementById("products__list");
+
+Product = function(type, name, src, price, category) {
+  this.type = type;
   this.name = name;
   this.src = src;
   this.price = price;
+  this.category = category;
   return this;
 }
 
+function generateCards(type, itemCategory) {
+  products.forEach((element) => {
+    if (element.type === type) {
+      if (element.category.indexOf(itemCategory) !== -1) {
+        // Выводим список товаров по темплейту
+        productList.innerHTML += `
+          <acticle class="products-card">
+            <header class="products-card__header">
+              <p class="products-card__category">
+                ${element.type}
+              </p>
+            </header>
+            <a class="products-card__link" href="#">
+              <img class="products-card__img" src="./img${"/pr_" + element.src + ".png"}">
+              <h3 class="products-card__name">
+                ${element.name}
+              </h3>
+            </a>
+            <footer class="products-card__footer">
+              <span class="products-card__price">
+                ${"$ " + element.price}
+              </span>
+            </footer>
+          </article>
+        `;
+      }
+    };
+  });
+};
+
 products = [
-  new Product("Recommend","Asus ZenFone", "asus-zenfone-go", 123),
-  new Product("Recommend","Apple iMac 27 Retina 5K", "apple-imac-27-retina-5k", 384),
-  new Product("Bestseller","Xiaomi Mi Band 2", "xiaomi-mi-band-2", 84),
-  new Product("Goods of week","LG 43UH610V", "lg-43uh610v", 310),
-  new Product("New product","LG Conditioner", "lg-conditioner", 104),
-  new Product("Recommend","Electrolux EZB 52410 AK", "electrolux", 360),
-  new Product("Bestseller","Beats studio wireless", "beats-studio-wireles", 210),
-  new Product("Recommend","Nikon D3300 Kit", "nikon-d3300", 302),
+  new Product("Recommend","Asus ZenFone", "asus-zenfone-go", 123, "Electronics"),
+  new Product("Recommend","Apple iMac 27 Retina 5K", "apple-imac-27-retina-5k", 384, "Electronics"),
+  new Product("Bestseller","Xiaomi Mi Band 2", "xiaomi-mi-band-2", 84, "Electronics, Children's goods"),
+  new Product("Goods of week","LG 43UH610V", "lg-43uh610v", 310, "Appleances"),
+  new Product("New product","LG Conditioner", "lg-conditioner", 104, "Appleances"),
+  new Product("Recommend","Electrolux EZB 52410 AK", "electrolux", 360, "Appleances"),
+  new Product("Bestseller","Beats studio wireless", "beats-studio-wireles", 210, "Electronics, childrens good"),
+  new Product("Recommend","Nikon D3300 Kit", "nikon-d3300", 302, "Electronics"),
 ];
 
-// Проверка вывода массива товаров
-console.log(products);
-
-// let categories = [];
-// Находим все кнопки
-// let btns = document.querySelectorAll(".product-header__btn");
-// console.log(btns);
-// // Смотрим каждую кнопку и возвращаем значение атрибутов
-// let categor = btns.forEach((elem) => {
-//   categories.push(elem.getAttribute("data-category"));
-// });
-// console.log(categories); // "popular", "trend", "new"
-
-products.forEach((element) => {
-  // Проверка вывода только категории Bestseller
-  if (element.category === "Bestseller") {
-    document.getElementById("products__list").innerHTML += `
-      <acticle class="products-card">
-        <header class="products-card__header">
-          <p class="products-card__category">
-            ${element.category}
-          </p>
-        </header>
-        <a class="products-card__link" href="#">
-          <img class="products-card__img" src="./img${"/pr_" + element.src + ".png"}">
-          <h3 class="products-card__name">
-            ${element.name}
-          </h3>
-        </a>
-        <footer class="products-card__footer">
-          <span class="products-card__price">
-            ${"$ " + element.price}
-          </span>
-        </footer>
-      </article>
-    `;
-  }
+// Срабатывает по клику на любой из элементов-кнопок
+// - считывает тип нужных товаров из нажатой кнопки
+// - очищает список товаров
+// - выводит список нужного типа
+typeButtons.forEach(function(elem) {
+  elem.addEventListener('click', function() {
+    // считывает тип товара
+    typeItem = this.getAttribute('data-category');
+    // Очищаем список товаров
+    productList.innerHTML = '';
+    itemCategory = document.getElementById("js-products-categories").value;
+    // Вываливаем товары в список
+    generateCards(typeItem, itemCategory);
+  });
 });
 
+// Если категория не выбрана, то
+// вываливаем весь массив продуктов на страницу
+// без фильтров
+function generateAllCard() {
+  if(document.getElementById("js-products-categories").value === "all") {
+    products.forEach((element) => {
+      productList.innerHTML += `
+        <acticle class="products-card">
+          <header class="products-card__header">
+            <p class="products-card__category">
+              ${element.type}
+            </p>
+          </header>
+          <a class="products-card__link" href="#">
+            <img class="products-card__img" src="./img${"/pr_" + element.src + ".png"}">
+            <h3 class="products-card__name">
+              ${element.name}
+            </h3>
+          </a>
+          <footer class="products-card__footer">
+            <span class="products-card__price">
+              ${"$ " + element.price}
+            </span>
+          </footer>
+        </article>
+      `;
+    });
+  };
+};
 
-
+// Функция вываливания всех продуктов на страницу
+generateAllCard();
